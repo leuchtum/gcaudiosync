0f45ed384399556d4833c187ac6ff4afd32928fb
Timo Haas <timo.haas@uni-ulm.de>
2024-03-18 11:26:53 +0100

    added folder scripts and moved files

---
+++ scripts/test_gcanalyser.py
@@ -0,0 +1,17 @@
+
+from pathlib import Path
+from gcaudiosync.gcanalyser.gcanalyser import GCodeAnalyser
+
+# links to the test-files
+src_path = Path("gcode")
+src_path_1 = src_path / "rechtecke.cnc"
+src_path_2 = src_path / "Getriebegehaeuse.tap"
+
+# create a G_Code_Analyser
+G_Code = GCodeAnalyser()
+
+# analyse G-Code
+G_Code.analyse(src_path_1)
+
+# print visualisation data
+print(G_Code.Data_visualisation)
