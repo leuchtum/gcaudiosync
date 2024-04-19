a751045c357c475f4ec0e5eb2b493177d7b17a33
Timo Haas <timo.haas@uni-ulm.de>
2024-04-19 15:48:35 +0200

    push on friday afternoon

--- gcaudiosync/gcanalyser/gcanalyser.py
+++ gcaudiosync/gcanalyser/gcanalyser.py
@@ -21,6 +21,20 @@ class GCodeAnalyser:
         # read in the g_code
         self.g_code = read_file(g_code_src)
 
+        # make something like an object for the current movement and the last movement
+        # make an object/list for: events
+        # objects in events: lin_movement, arc_movement, dewll, pause, spindle_change, tool_change
+        # objects for synchronisation: dewll_times/pause/tool_change, frequencys, S_change
+        # initialize start position
+
+        # for loop over all lines
+
+            # for loop over all information in the line
+
+            # g -> 
+            # x,y,z,i,j,k,r
+            # m
+
         return 0
     
 
