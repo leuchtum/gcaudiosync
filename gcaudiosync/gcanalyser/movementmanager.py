bugfix and improved time computation

gcaudiosync/gcanalyser/movementmanager.py
@@ -51,16 +51,16 @@ class MovementManager:
             CNC parameter instance.
         """
 
-        self.start_time : int                = 0         # Time for the start
-        self.total_time: int                 = 0         # Expected total time
-        self.movements: List[Movement]       = []        # Movements
-        self.end_of_program_reached: bool    = False     # Variable for end of program
+        self.start_time : float             = 0         # Time for the start
+        self.total_time: float              = 0         # Expected total time
+        self.movements: List[Movement]      = []        # Movements
+        self.end_of_program_reached: bool   = False     # Variable for end of program
         
-        self.CNC_Parameter = CNC_Parameter              # Get cnc parameter
+        self.CNC_Parameter: CNCParameter    = CNC_Parameter              # Get cnc parameter
 
         # Get max acceleration and deceleration
-        self.max_acceleration = np.zeros(3)
-        self.max_deceleration = np.zeros(3)
+        self.max_acceleration: np.array     = np.zeros(3)
+        self.max_deceleration: np.array     = np.zeros(3)
         self.get_acceleration_and_deceleration()
 
         # Create the initial CNC_Status object for the initialization of the MovementManager object
@@ -186,11 +186,11 @@ class MovementManager:
         last_movement: Movement = copy.deepcopy(self.movements[-1])         
 
         # Get current positions
-        current_position_linear_axes = last_movement.end_position_linear_axes.get_as_array()        
-        current_position_rotation_axes = last_movement.end_position_rotation_axes.get_as_array() 
+        current_position_linear_axes = last_movement.end_position_linear_axes
+        current_position_rotation_axes = last_movement.end_position_rotation_axes
 
         # Get tool change position
-        tool_change_position_linear = self.CNC_Parameter.TOOL_CHANGE_POSITION_LINEAR_AXES.get_as_array()    
+        tool_change_position_linear = self.CNC_Parameter.TOOL_CHANGE_POSITION_LINEAR_AXES
 
         # Create movement to tool
         movement_get_tool = Movement(g_code_line_index = line_index, 
@@ -200,7 +200,7 @@ class MovementManager:
                                      start_position_rotation_axes = current_position_rotation_axes,
                                      end_position_rotation_axes = current_position_rotation_axes, 
                                      arc_information = None,
-                                     feed_rate = self.CNC_Parameter.F_MAX,
+                                     feed_rate = self.CNC_Parameter.F_MAX/60000.0,
                                      active_plane = 17)
         
         # Add exact stop
@@ -236,10 +236,10 @@ class MovementManager:
         # create new movement
         new_movement = Movement(g_code_line_index = line_index, 
                                 movement_type = -1, 
-                                start_position_linear_axes = copy.deepcopy(last_movement.end_position_linear_axes), 
-                                end_position_linear_axes = copy.deepcopy(last_movement.end_position_linear_axes),
-                                start_position_rotation_axes = copy.deepcopy(last_movement.end_position_linear_axes),
-                                end_position_rotation_axes = copy.deepcopy(last_movement.end_position_rotation_axes), 
+                                start_position_linear_axes = last_movement.end_position_linear_axes, 
+                                end_position_linear_axes = last_movement.end_position_linear_axes,
+                                start_position_rotation_axes = last_movement.end_position_linear_axes,
+                                end_position_rotation_axes = last_movement.end_position_rotation_axes, 
                                 arc_information = None,
                                 feed_rate = 0,
                                 active_plane = 17)
@@ -444,8 +444,10 @@ class MovementManager:
                                             start_vector_linear_axes_next_movement)
                 if factor >= 1:
                     start_and_end_vector = start_vector_linear_axes_next_movement
-                else:
+                elif factor > 0:
                     start_and_end_vector = end_vector_linear_axes_current_movement
+                else: 
+                    pass
             else:
                 pass
             

gcaudiosync/gcanalyser/rotationaxes.py
@@ -10,9 +10,9 @@ class RotationAxes:
                  B: float = 0.0,
                  C: float = 0.0):
         
-        self.A = A
-        self.B = B
-        self.C = C
+        self.A: float = A
+        self.B: float = B
+        self.C: float = C
 
     #################################################################################################
     # Methods
@@ -39,7 +39,7 @@ class RotationAxes:
         """
 
         array = np.array([self.A, self.B, self.C])
-        return copy(array)
+        return copy.copy(array)
     
     def print(self):
         """

gcaudiosync/gcanalyser/vectorfunctions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 import math
 
 def compute_normal_vector(vec1, 
@@ -64,14 +65,32 @@ def get_factor(vec1, vec2):
     factor = 0
 
     for index in range(len(vec1)):
-        if vec1[index] != 0:
+        if vec2[index] != 0:
             factor = vec1[index] / vec2[index]
             break
     
     return factor
 
 # TODO: comment
-def compute_angle(vec1: np.ndarray, vec2: np.ndarray, smaller_angle: np.bool_) -> float:
+def compute_smaller_angle_in_degree(vec1: np.ndarray, vec2: np.ndarray) -> float:
+
+    # Handle potential division by zero
+    if np.linalg.norm(vec1) == 0 or np.linalg.norm(vec2) == 0:
+        raise ValueError("Cannot calculate angle for zero-norm vectors.")
+
+    # Calculate cosine of the angle
+    cosine = np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+
+    # Ensure cosine value is within valid range (-1, 1)
+    cosine = np.clip(cosine, -1, 1)
+
+    # Calculate the angle in degrees
+    angle_degrees = np.degrees(np.arccos(cosine))
+
+    return angle_degrees
+
+# TODO: comment
+def compute_small_or_big_angle_in_degree(vec1: np.ndarray, vec2: np.ndarray, smaller_angle: np.bool_) -> float:
     """
     Calculates the smaller or bigger angle between two NumPy arrays in degrees.
 
@@ -87,21 +106,28 @@ def compute_angle(vec1: np.ndarray, vec2: np.ndarray, smaller_angle: np.bool_) -
         ValueError: If the input vectors are not valid NumPy arrays or have zero norm.
     """
 
-    # Handle potential division by zero
-    if np.linalg.norm(vec1) == 0 or np.linalg.norm(vec2) == 0:
-        raise ValueError("Cannot calculate angle for zero-norm vectors.")
-
-    # Calculate cosine of the angle
-    cosine = np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
-
-    # Ensure cosine value is within valid range (-1, 1)
-    cosine = np.clip(cosine, -1, 1)
-
-    # Calculate the angle in degrees
-    angle_degrees = np.degrees(np.arccos(cosine))
+    smaller_angle_degrees = compute_smaller_angle_in_degree(vec1 = vec1, vec2 = vec2)
 
     # Return requested angle
-    return min(angle_degrees, 360 - angle_degrees) if smaller_angle else max(angle_degrees, 360 - angle_degrees)
+    return smaller_angle_degrees if smaller_angle else 360 - smaller_angle_degrees
+
+def compute_small_or_big_angle(vec1: np.ndarray, vec2: np.ndarray, smaller_angle: np.bool_) -> float:
+    angle_degree = compute_smaller_angle_in_degree(vec1, vec2)
+
+    return angle_degree / 360.0 * 2 * math.pi
+
+# TODO: comment
+def compute_angle_from_X_axis(vec: np.array) -> float:
+
+    if len(vec) != 2:
+        raise Exception(f"Vector must be 2D")
+    
+    smaller_angle_degree = compute_smaller_angle_in_degree(vec1 = np.array([1, 0]), vec2 = copy.copy(vec))
+
+    if vec[1] >= 0:
+        return smaller_angle_degree
+    else:
+        return 360 - smaller_angle_degree
 
 # TODO: comment
 def rotate_2D_vector(center_2_point, center, angle):
@@ -114,8 +140,10 @@ def rotate_2D_vector(center_2_point, center, angle):
     return new_vec
 
 # TODO: comment
-def normalize(vec):
+def normalize(vec_in):
     
+    vec = copy.copy(vec_in)
+
     if np.linalg.norm(vec) == 0:
         return vec
     