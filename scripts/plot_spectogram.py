fe0aad05cacbccd9dc56aa8fa9fa462c3ebd9574
Daniel Müller <acct.d-mueller@mailbox.org>
2024-08-15 13:01:32 +0200

    plot now supports lazypreprocessing and consts

--- scripts/plot_spectogram.py
+++ scripts/plot_spectogram.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from matplotlib import pyplot as plt
 
+from gcaudiosync.audioanalyser.constants import Constants
 from gcaudiosync.audioanalyser.io import RawRecording
 from gcaudiosync.audioanalyser.signalprocessing import LazyProcessedRecording
 from gcaudiosync.audioanalyser.visualize import add_footnote, spectrogram
@@ -10,14 +11,20 @@ from gcaudiosync.audioanalyser.visualize import add_footnote, spectrogram
 
 def run(file: Path):
     rr = RawRecording.from_file(file)
-    pr = LazyProcessedRecording(data=rr.data, samplerate=rr.samplerate)
+    consts = Constants(rr.samplerate, rr.data)
+    pr = LazyProcessedRecording(
+        rr.data,
+        n_fft=consts.n_fft,
+        hop_length=consts.hop_length,
+        win_length=consts.win_length,
+    )
 
     X = pr.S_db()
     span = X.max() - X.min()
     X[X < (X.min() + 0.33 * span)] = X.min()
     X[X > (X.min() + 0.66 * span)] = X.max()
     fig, ax = plt.subplots(figsize=(14, 8))
-    ax = spectrogram(pr.S_db(), pr.samplerate, is_db=True, y_axis="log", ax=ax)
+    ax = spectrogram(pr.S_db(), consts, is_db=True, y_axis="linear", ax=ax)
     note = f"File: {file.name}\n" f"X.shape: {pr.S_db().shape}"
     ax = add_footnote(ax, note, loc="left")
     fig = ax.get_figure()
@@ -26,7 +33,7 @@ def run(file: Path):
 
 
 if len(sys.argv) == 1:
-    file = Path("generated_tones/tone2.wav")
+    file = Path("sound/VID_20240103_125230.wav")
     run(file)
 else:
     for file in sys.argv[1:]:
