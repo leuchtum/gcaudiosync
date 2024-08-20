85826b8b0ac9a5b85e3231aeed0b808e35a35245
Timo Haas <timo.haas@uni-ulm.de>
2024-08-20 16:32:42 +0200

    added comments

--- gcaudiosync/gcanalyser/toolpathinformation.py
+++ gcaudiosync/gcanalyser/toolpathinformation.py
@@ -3,10 +3,26 @@ import numpy as np
 from gcaudiosync.gcanalyser.linearaxes import LinearAxes
 
 class ToolPathInformation:
+    """
+    Contains information about the toolpath
 
-    g_code_line_index: int = 0
-    position_linear_axes: np.array = np.zeros(3)
-    movement_type: int = 0
-
+    Attributes:
+    ----------
+        g_code_line_index : int
+            indec of the g-code-line
+        position_linear_axes : np.array
+            X, Y and Z coordinates of the position
+        movement_type : int
+            movement-type: 
+            0: rapid linear movement
+            1: linear movement
+            2: arc CW
+            3: arc CCW
+    """
     def __init__(self):
-        pass
+        """
+        Initializes the ToolPathInformation object
+        """
+        self.g_code_line_index: int = 0
+        self.position_linear_axes: np.array = np.zeros(3)
+        self.movement_type: int = 0
