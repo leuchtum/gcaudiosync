26a7dd14b66eb7142adf4a11eadbc1cd2a59dfbb
Daniel Müller <acct.d-mueller@mailbox.org>
2024-08-21 07:11:56 +0200

    adapt test_gcanalyser for alternative animator

--- scripts/test_gcanalyser.py
+++ scripts/test_gcanalyser.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 
+from matplotlib import pyplot as plt
+
 # Import the G-Code-Analyser
 from gcaudiosync.gcanalyser.gcanalyser import GCodeAnalyser
+from gcaudiosync.gcanalyser.toolpathgenerator import AlternativeToolPathAnimator
 
 # Links to the test-file
 src_path_g_code = Path("gcode")
@@ -12,8 +15,9 @@ src_path_cnc_parameter = Path("readinfiles") / "parameter.txt"
 src_snapshot_g_code = Path("readinfiles") / "snapshot_g_code.txt"
 
 # Create a G_Code_Analyser
-G_Code_Analyser = GCodeAnalyser(parameter_src = src_path_cnc_parameter, 
-                                snapshot_src = src_snapshot_g_code)
+G_Code_Analyser = GCodeAnalyser(
+    parameter_src=src_path_cnc_parameter, snapshot_src=src_snapshot_g_code
+)
 
 # Analyse G-Code
 G_Code_Analyser.analyse(src_path_1)
@@ -42,6 +46,22 @@ G_Code_Analyser.Sync_Info_Manager.frequency_info()
 G_Code_Analyser.Sync_Info_Manager.snapshot_info()
 
 # Generate and plot toolpath
-G_Code_Analyser.generate_tool_path(fps = 10)
-G_Code_Analyser.plot_tool_path(version = "Haas")
+use_alternative_toolpath_animator = True
 
+if use_alternative_toolpath_animator:
+    fig = plt.figure(figsize=(8, 5))
+    ax_info = fig.add_subplot(1, 3, 1)
+    ax_tool = fig.add_subplot(1, 3, (2, 3))
+
+    toolpath_ani = AlternativeToolPathAnimator(
+        G_Code_Analyser.Movement_Manager,
+        G_Code_Analyser.g_code,
+        ax_tool,
+        ax_info,
+        60,
+    )
+    toolpath_ani.run()
+    plt.show()
+else:
+    G_Code_Analyser.generate_tool_path(fps=10)
+    G_Code_Analyser.plot_tool_path(version="Haas")
