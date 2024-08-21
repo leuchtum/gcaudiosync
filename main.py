566387b1948b43dad239dd4111446d97e88664bd
Daniel Müller <acct.d-mueller@mailbox.org>
2024-08-21 08:49:18 +0200

    Removed unused files

--- main.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-import gcaudiosync.io
-
-file_path = Path("/Users/daniel/Desktop/WD_cse_project/VID_20240103_125230_MONO.wav")
-
-sound_data = gcaudiosync.io.read_wav_file(file_path)
-print(sound_data.nchannels)
